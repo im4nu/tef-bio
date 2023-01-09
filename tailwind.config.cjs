881e5b00fb901a2a9541ed04dedad4a9b7963df4
Emmanuel victor <victorx811@gmail.com>
2023-01-09 10:18:01 -0300

    try fix instagram bg issue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,10 +7,10 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'instagram': "url('/src/assets/images/insta.png')",
-        'linkedin': "url('/src/assets/images/linkedin.png')",
-        'whatsapp': "url('/src/assets/images/whats.png')",
-        'gmail': "url('/src/assets/images/mail.png')",
+        'instagram': "url('./src/assets/images/insta.png')",
+        'linkedin': "url('./src/assets/images/linkedin.png')",
+        'whatsapp': "url('./src/assets/images/whats.png')",
+        'gmail': "url('./src/assets/images/mail.png')",
       }
 
     },
